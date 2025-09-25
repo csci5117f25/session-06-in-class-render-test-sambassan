4d74334e9f61c9773d5b49da8184e5e3dd1086f5
Sam Bassan <bassa050@umn.edu>
2025-09-25 12:53:31 -0500

    Added form and connected form to database for session 8

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+import db
 
 app = Flask(__name__)
+db.setup()
 
 @app.route('/')
+
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello.html', name=name, guestbook=db.get_guestbook())
+
+@app.post('/submit') 
+def submit():
+    name = request.form.get("name")
+    text = request.form.get("message")
+    db.add_post(name, text)
+    return redirect('/')
