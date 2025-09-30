97e19f583f0479a659542a94e376311f0a35e0e6
Sam Bassan <bassa050@umn.edu>
2025-09-30 12:42:19 -0500

    added sessions to store name in url

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-from flask import Flask, redirect, render_template, request
-import db
+from flask import Flask, redirect, render_template, request, session
+import db, os
 
 app = Flask(__name__)
 db.setup()
+app.secret_key = os.environ['FLASK_SECRET']
 
 @app.route('/')
 
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name, guestbook=db.get_guestbook())
+    if name and name != 'favicon.ico':
+        session['name'] = name
+    current_name = session['name']
+    return render_template('hello.html', name=current_name, guestbook=db.get_guestbook())
 
 @app.post('/submit') 
 def submit():
